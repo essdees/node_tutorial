573f73cfdfc5139f8afa78a79f98510dd88b03e2
Snehdeep Singh <snehdeep.singh@trajectorservices.com>
2024-07-08 16:51:19 +0530

    first route configured

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
-connectDB(() => {
-  app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running at port: ${process.env.PORT}`);
-  });
-})
-  .then()
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
   .catch((err) => {
     console.log("MONGO DB connection failed !!!", err);
   });
